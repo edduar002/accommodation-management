0ce59a758f987d08cbc77db7f0e0adc279529971
edduar002 <128913974+edduar002@users.noreply.github.com>
2025-09-01 13:07:42 -0500

    Se inicia el proyecto, empezando con la creación de la carpeta del proyecto junto con dependencias

---
+++ src/main/java/com/uniquindio/proyecto_final/accommodation_management/AccommodationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.proyecto_final.accommodation_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommodationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniquindio/proyecto_final/accommodation_management/AccommodationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.proyecto_final.accommodation_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
